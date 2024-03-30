21bb94b14ed7c609add956e4109f3328a9578914
Muny1re1 <munyiriandy@gmail.com>
2024-03-30 14:49:39 +0300

    Filter lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+const drivers = [
+    { name: 'John Smith' },
+    { name: 'Jane Doe' },
+    { name: 'Mike Johnson' }
+  ];
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+};
+console.log(findMatching(drivers, "Lewis Hamilton"));
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name);
+}
